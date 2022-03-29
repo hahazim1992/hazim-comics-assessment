0da3b3f2447a1330a1f9b208b7964576953d5dae
hahazim1992 <hahazim1992@gmail.com>
2022-03-29 23:28:26 +0800

    refactor interface N API comsumption

--- src/app/model/character.ts
+++ src/app/model/character.ts
@@ -1,7 +1,12 @@
 export interface Character {
-    /* date_last_updated: string */
-    error: string,
-    aliases: string;
+    date_last_updated: string
+    name: string;
+    real_name: string;
+    deck: string;
+    image: {
+        icon_url: string;
+    };
+    id: number;
     /* name
 real name 
 the brief summary of the character
