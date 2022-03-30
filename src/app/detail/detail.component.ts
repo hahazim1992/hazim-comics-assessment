ddd120a3f475c077cb9b401ac8dc3b25263abf65
hahazim1992 <hahazim1992@gmail.com>
2022-03-31 01:28:27 +0800

    fixed routing from home to details page

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Character } from '../model/character';
 import { CharacterService } from '../service/character.service';
 import { ActivatedRoute, ParamMap } from '@angular/router';
-import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-detail',
@@ -11,36 +10,21 @@ import { switchMap } from 'rxjs';
 })
 export class DetailComponent implements OnInit {
   character: any = [];
-  //loading: boolean = true;
-  bid: number = 1475;
+  bid: any;
   constructor(
     private characterService: CharacterService,
     private activatedRoute: ActivatedRoute,
   ) { }
 
   ngOnInit(): void {
-    //this.getDetail(this.bid);
-    //this.getCharacter();
-    this.activatedRoute.paramMap.pipe(
-      switchMap((params: ParamMap) =>
-      this.characterService.getDetail(params.get('id')!))
-    );
+    this.bid = this.activatedRoute.snapshot.paramMap.get('id');
+    this.getDetail(this.bid);
   }
 
-/*    getDetail(id: any){
+   getDetail(id: any){
     this.characterService.getDetail(id).subscribe((data: Character[]) => {
       this.character = data['results' as any ];
-      //this.loading = false;
       console.log(this.character)
     })
-  }  */
-
-/*   getCharacter(){
-    this.characterService.getCharacter().subscribe((data: Character[]) => {
-      this.character = data['results' as any ];
-      //this.loading = false;
-      console.log(this.character)
-    })
-  } */
-
+  }
 }
